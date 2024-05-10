94a73bff99c48b0660fb8f9741d1b12fea1fa87b
Plangloi <48372629+Plangloi@users.noreply.github.com>
2024-05-10 11:58:18 -0400

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -2,21 +2,54 @@
 
 # Step 1: Define your aliases
 aliases=(
-	"alias ll='ls -alF'"
-	"alias grep='grep --color=auto'"
-	"alias ..='cd ..'"
-	"alias ...='cd ../../../'"
-	"alias ll='ls -la'"
-	"alias cd..='cd ..'"
-	"alias mkdir='mkdir -pv'"
-	"alias vi='vim'"
-	"alias svi='sudo vi'"
-	"alias edit='vim'"
-	"alias nv='nvim'"
-	"alias ping='ping -c 6'"
-	"alias ports='netstat -tulanp'"
-	"alias ports='netstat -tulanp'"
 
+"alias_grep='grep --color=auto'" # Search for a pattern inside a file (with color highlighting)
+
+"alias_cd_up='cd ..'" # Navigate up one directory level
+
+"alias_cd_up_up='cd ../..'" # Navigate up two directory levels
+
+"alias_ll='ls -la'" # List all files and directories (including hidden ones) with detailed information
+
+"alias_cd_up_again='cd ..'" # Navigate up one directory level (duplicate of alias_cd_up)
+
+"alias_mkdir='mkdir -pv'" # Create a directory and any necessary parent directories, with verbose output
+
+"alias_vi='vim'" # Open the Vim text editor
+
+"alias_sudo_vi='sudo vi'" # Open the Vim text editor with superuser privileges
+
+"alias_edit='vim'" # Open the Vim text editor (duplicate of alias_vi)
+
+"alias_nvim='nvim'" # Open the Neovim text editor
+
+"alias_ping='ping -c 6'" # Ping a host with a limit of 6 packets
+
+"alias_ports='netstat -tulanp'" # List all listening ports and associated processes
+
+"alias_apt_get='sudo apt-get'" # Run the apt-get command with superuser privileges
+
+"alias_apt_get_yes='sudo apt-get --yes'" # Run the apt-get command with superuser privileges and automatically answer "yes" to prompts
+
+"alias_update='sudo apt-get update && sudo apt-get upgrade'" # Update package lists and upgrade installed packages
+
+"alias_reboot='sudo /sbin/reboot'" # Reboot the system with superuser privileges
+
+"alias_poweroff='sudo /sbin/poweroff'" # Power off the system with superuser privileges
+
+"alias_halt='sudo /sbin/halt'" # Halt the system with superuser privileges
+
+"alias_shutdown='sudo /sbin/shutdown'" # Shutdown the system with superuser privileges
+
+"alias_cpuinfo='lscpu'" # Display CPU information
+
+"alias_df='df -H'" # Display disk usage with human-readable units
+
+"alias_du='du -ch'" # Display disk usage with human-readable units and a grand total
+
+"alias_http='python3 -m http.server 8080'" # Start a simple HTTP server on port 8080
+
+"alias_me='hostname -I'" # Display the IP address of the current host
 )
 
 # Check if .bashrc file exists
